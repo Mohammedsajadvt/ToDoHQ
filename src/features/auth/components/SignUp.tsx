d61905525c09215c52f3b10e22d95886a9f38203
Mohammed Sajad VT <Sajad.vt@kiebot.com>
2025-07-03 23:08:59 +0530

    Added font

--- src/features/auth/components/SignUp.tsx
+++ src/features/auth/components/SignUp.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import signup from "../../../assets/signup.png";
+import { Input } from '../../../components/ui/input';
 function SignUp() {
 
 
-    return (
-        <div className='flex'>
-          <img className='h-auto max-w-xs absolute left-0 bottom-0' src={signup} alt="" />
-        </div>
-    )
+  return (
+    <div className='flex'>
+      <img className='h-auto max-w-xs absolute left-0 bottom-0' src={signup} alt="" />
+      <div className='flex-col absolute top-6 '>
+        <h2 className=' font-bold text-3xl'>SignUp</h2>
+        <Input type='email'/>
+      </div>
+    </div>
+  )
 }
 
 export default SignUp
